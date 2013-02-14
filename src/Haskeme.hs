54935b3dec1649423b58eb40e848de60b589526c
Arian van Putten <aeroboy94@gmail.com>
2013-02-14 22:05:26 +0100

    parse strings

--- src/Haskeme.hs
+++ src/Haskeme.hs
@@ -1,3 +1,22 @@
-import Text.Parsec
+module Haskeme(main) where
+
+import Text.Parsec hiding (spaces)
+import Text.Parsec.String
+import Control.Applicative
+
+data Value = Atom String
+           | List [Value]
+           | DottedList [Value] Value
+           | Number Integer
+           | String String
+           | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%|*=-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 
+parseString :: Parser Value
+parseString = String <$> (char '"' *> (manyTill anyChar $ try $ char '"'))
