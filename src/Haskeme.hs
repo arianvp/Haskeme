9fe0c304d160aa4f5c7a248049e44e04efbd570e
Arian van Putten <aeroboy94@gmail.com>
2013-02-22 00:05:54 +0100

    implement parser by following spec

--- src/Haskeme.hs
+++ src/Haskeme.hs
@@ -2,60 +2,67 @@ module Main(main) where
 
 import Text.Parsec hiding (spaces)
 import Text.Parsec.String
-import qualified Text.Parsec.Token as P
+import qualified Text.Parsec.Token as T
+import Text.Parsec.Language (emptyDef)
 import Data.Char
 import Control.Applicative hiding ((<|>), many)
 
-data Value = Atom String
-           | List [Value]
-           | DottedList [Value] Value
-           | Number Integer
-           | String String
-           | Bool Bool deriving (Show)
+-- parseDottedList :: Parser Value
+-- parseDottedList = DottedList <$> (endBy parseExpr spaces) <*> (char '.' *> spaces *> parseExpr)    
 
-symbol :: Parser Char
-symbol = oneOf "!#$%|*=-/:<=>?@^_~"
+-- parseQuoted :: Parser Value
+-- parseQuoted = List <$> ((Atom "quote") :) <$> (: []) <$> (char '\'' *> parseExpr)
 
-spaces :: Parser ()
-spaces = skipMany1 space
 
-notDigit :: Parser Char
-notDigit = satisfy $ not <$> isDigit
 
-parseString :: Parser Value
-parseString = String <$> (char '"' *> (manyTill anyChar $ try $ char '"'))
+-- http://rose-r5rs.googlecode.com/hg/doc/r5rs-grammar.html
+lexer :: T.TokenParser ()
+lexer = T.makeTokenParser emptyDef
+        { T.commentLine     = ";"
+        , T.identStart      = initial
+        , T.identLetter     = subsequent
+        , T.reservedOpNames = ["+", "-"]
+        , T.reservedNames   = ["quote", "lambda", "if"
+                            ,"set!", "begin", "cond", "and", "or", "case"
+                            , "let", "let*", "letrec", "do", "delay"
+                            , "quasiquote", "else", "=>", "define"
+                            , "unquote", "unquote-splicing"
+                            ]
+        }
+        where initial     = letter <|> initial'
+              initial'    = oneOf "!$%&*/:<=>?^_~"
+              subsequent  = initial <|> digit <|> subsequent'
+              subsequent' = oneOf"+-.@"
 
-parseNumber :: Parser Value
-parseNumber = Number <$> read <$> many1 digit <* notFollowedBy (alphaNum <|> symbol)
+data Expr = Bool Bool
+          | String String
+          | Integer Integer
+          | Atom String
+          | List [Expr]
+          | DottedList [Expr] Expr deriving (Show)
 
-parseBool :: Parser Value
-parseBool =  string "#t" *> pure (Bool True)
-         <|> string "#f" *> pure (Bool False)
+whiteSpace = T.whiteSpace lexer
+parens = T.parens lexer
 
-parseAtom :: Parser Value
-parseAtom = Atom <$> (notFollowedBy (digit) *> many (letter <|> digit <|> symbol))
+bool :: Parser Expr
+bool =  Bool <$> (char '#' *> (char 't' *> pure True  <|> char 'f' *> pure False))
 
-parseExpr :: Parser Value
-parseExpr =  char '(' *>
-                try parseDottedList <|> parseList
-             <* char ')'
-         <|> parseNumber
-         <|> parseString
-         <|> parseBool
-         <|> parseQuoted
-         <|> parseAtom
+stringLiteral :: Parser Expr
+stringLiteral = String <$> T.stringLiteral lexer
 
-parseList :: Parser Value
-parseList = List <$> sepBy parseExpr spaces
+integer :: Parser Expr
+integer = Integer <$> T.integer lexer
 
-parseDottedList :: Parser Value
-parseDottedList = DottedList <$> (endBy parseExpr spaces) <*> (char '.' *> spaces *> parseExpr)    
+list :: Parser Expr
+list = List <$> expr `sepBy` whiteSpace
 
-parseQuoted :: Parser Value
-parseQuoted = List <$> ((Atom "quote") :) <$> (: []) <$> (char '\'' *> parseExpr)
+atom :: Parser Expr
+atom = Atom <$> T.identifier lexer
 
-main = getLine >>= parseTest parseExpr
-  
+expr :: Parser Expr
+expr =  (parens list) <|> stringLiteral <|> integer <|> bool <|> atom
+
+main = getLine >>= parseTest (whiteSpace *> expr <* eof)
 
 
 
